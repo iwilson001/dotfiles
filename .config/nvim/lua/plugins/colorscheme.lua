d6ea54fec9546e3c234d8c3b92fbd0fb7047c51c
Ian Wilson <61028059+iwilson001@users.noreply.github.com>
2026-01-02 13:46:31 -0600

    Update gruvbox colorscheme configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,9 @@ return {
 	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
 	config = function()
+		require("gruvbox").setup({
+			contrast = "soft",
+		})
 		vim.cmd("colorscheme gruvbox")
 	end,
-	opts = { contrast = "soft" },
 }
