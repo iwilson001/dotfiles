3df362d4762faf737e8b727f8171c9fcc89cf5e5
ian <ianw2000ct@gmail.com>
2026-01-05 19:37:30 -0600

    updates copilot add word

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -14,25 +14,16 @@ return {
 					auto_trigger = true,
 					keymap = {
 						accept = "<Tab>",
-						accept_word = "",
+						accept_word = "<C-l>",
 					},
 				},
 			})
 		end,
-		-- opts = {
-		-- 	-- suggestion = { enabled = false },
-		-- 	-- panel = { enabled = false },
-		-- 	-- filetypes = {
-		-- 	-- 	markdown = true,
-		-- 	-- 	help = true,
-		-- 	-- },
-		-- },
 	},
 
 	{
 		"saghen/blink.cmp",
 		version = "1.*",
-		-- dependencies = { "fang2hou/blink-copilot" },
 		opts = {
 			keymap = {
 				preset = "enter",
@@ -42,21 +33,11 @@ return {
 			fuzzy = { implementation = "prefer_rust" },
 			sources = {
 				default = { "lsp", "buffer", "snippets", "path" },
-				-- providers = {
-				-- 	copilot = {
-				-- 		name = "copilot",
-				-- 		module = "blink-copilot",
-				-- 		score_offset = 100,
-				-- 		async = true,
-				-- 	},
-				-- },
 			},
 			completion = {
 				menu = {
 					auto_show = true,
 				},
-				-- TODO: see if we want this or nah
-				-- Show documentation when selecting a completion item
 				documentation = { auto_show = true, auto_show_delay_ms = 300 },
 				list = {
 					selection = { preselect = true, auto_insert = false },
