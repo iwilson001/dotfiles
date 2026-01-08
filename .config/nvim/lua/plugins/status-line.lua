ce07e179ad81031c102bb15fd728c46fcc549516
ian <ianw2000ct@gmail.com>
2026-01-07 19:11:41 -0600

    removes unused status line parts
    - tried bufferline and I don't see the point

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -36,10 +36,6 @@ return {
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff" },
 					lualine_c = { all_buffer_diagnostics, { "diagnostics", always_visible = true } },
-					-- lualine_c = { {
-					-- 	"diagnostics",
-					-- 	always_visible = true,
-					-- } },
 					lualine_x = { "encoding", "fileformat", "filetype" },
 					lualine_y = { "progress" },
 					lualine_z = { "location" },
@@ -52,13 +48,8 @@ return {
 							mode = 2, -- buffer name + index
 							max_length = vim.o.columns,
 							buffers_color = {
-								-- -- Same values as the general color option can be used here.
-								-- active = "lualine_a_normal", -- Color for active buffer.
-								-- inactive = "lualine_a_inactive", -- Color for inactive buffer.
-								active = { fg = "#fabd2f", bg = "#32302f", gui = "bold" }, -- more contrast
+								active = { fg = "#fabd2f", bg = "#32302f" }, -- more contrast
 								inactive = { fg = "#a89984", bg = "#3c3836" },
-								-- active = { fg = "#fabd2f", bg = "#504945", gui = "bold" }, -- active buffer
-								-- inactive = { fg = "#d5c4a1", bg = "#3c3836" },
 							},
 						},
 					},
