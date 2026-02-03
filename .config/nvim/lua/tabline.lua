ef486a018ac978cf58db15dac908d813932ac203
Ian Wilson <61028059+iwilson001@users.noreply.github.com>
2026-02-03 14:18:02 -0600

    Rename render function and update tabline setting

--- .config/nvim/lua/tabline.lua
+++ .config/nvim/lua/tabline.lua
@@ -8,9 +8,25 @@ local function listed_buffers()
 	return buffers
 end
 
-function _G.a_render()
+-- local GLOBAL_HIGHLIGHT_NAMESPACE_ID = 0
+-- local TABLINE_PREFIX = "TabLine"
+-- vim.api.nvim_set_hl(GLOBAL_HIGHLIGHT_NAMESPACE_ID, TABLINE_PREFIX .. "Active", {
+-- 	fg = "#ffffff",
+-- 	bg = "#005f87",
+-- 	bold = true,
+-- })
+
+function _G.render()
 	local tabline = ""
 	for _, bufnr in ipairs(listed_buffers()) do
+		local is_current_buffer = bufnr == vim.api.nvim_get_current_buf()
+
+		if is_current_buffer then
+			tabline = tabline .. "%#TabLineSel#"
+		else
+			tabline = tabline .. "%#TabLine#"
+		end
+
 		local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":t")
 		tabline = tabline .. " " .. name .. " "
 	end
@@ -19,5 +35,5 @@ end
 
 -- TODO: comment in when done
 vim.o.showtabline = 2
-vim.o.tabline = "%!v:lua.a_render()"
+vim.o.tabline = "%!v:lua.render()"
 vim.cmd("redrawtabline")
