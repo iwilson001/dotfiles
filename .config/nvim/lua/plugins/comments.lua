795073bfa407ee14b6f712d8328cc777c069ffff
ian <ianw2000ct@gmail.com>
2026-03-01 18:52:03 -0600

    updates comment plugin

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,22 +1,3 @@
 return {
-	-- TODO: this doesn't even appear to do anything in the context of react so idk if
-	"numToStr/Comment.nvim",
-	config = function()
-		require("Comment").setup({
-			pre_hook = function()
-				return vim.bo.commentstring
-			end,
-
-			mappngs = {
-				basic = true,
-				extra = false,
-			},
-			ignore = "^$",
-		})
-	end,
-	lazy = false,
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		"nvim-treesitter/nvim-treesitter",
-	},
+	"folke/ts-comments.nvim",
 }
